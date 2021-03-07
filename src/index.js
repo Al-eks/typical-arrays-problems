41455ff2f82bfb01eb041c15f9c0cd214a9fcf1b
Alex <vouchankaaleks@gmail.com>
2021-03-07 15:15:37 +0300

    feat: implement function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
-  return 0;
+
+  if ((array == undefined) || (array.length === 0)) return [];
+
+  let minValue = 0;
+  for( i = 0; i < array.length; i++) if( array[i] < minValue) minValue = array[i];
+
+  return minValue;
 }
 
 exports.max = function max (array) {
-  return 0;
+
+  if ((array == undefined) || (array.length === 0)) return [];
+
+  let maxValue = 0;
+  for( i = 0; i < array.length; i++) if( array[i] > maxValue) maxValue = array[i];
+
+  return maxValue;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+
+  if ((array == undefined) || (array.length === 0)) return [];
+
+  let avgValue = array.reduce((a, b) => (a + b)) / array.length;
+
+  return avgValue;
 }
